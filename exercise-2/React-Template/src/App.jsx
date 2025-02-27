4b16e0ea4cefb8a69708ecf73e6a18986b06ef06
Pychey <prakpycheyy@gmail.com>
2025-02-27 14:25:36 +0700

    Complete Exercise-2 (Convert Vanila HTML to ReactJS)

--- exercise-2/React-Template/src/App.jsx
+++ exercise-2/React-Template/src/App.jsx
@@ -2,6 +2,11 @@ import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
 
+//Main Difference between HTML and JSX syntax
+//Embedding Expressions: JSX allows embedding JS expression
+//Components: JSX support the creation of reusable componenets
+//Conditional Rendering: JSX supports conditional rendering through JavaScript expressions, allowing components to render different content based on conditions 
+
 function App() {
   return (
     <>
