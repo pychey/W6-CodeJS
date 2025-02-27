c6b2c4a9e24d0a7f320453220463eb5999272a1c
Pychey <prakpycheyy@gmail.com>
2025-02-27 14:32:12 +0700

    Complete Exercise-2 (Convert Vanila HTML to ReactJS)

--- exercise-2/React-Template/src/App.jsx
+++ exercise-2/React-Template/src/App.jsx
@@ -2,10 +2,22 @@ import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
 
-//Main Difference between HTML and JSX syntax
-//Embedding Expressions: JSX allows embedding JS expression
-//Components: JSX support the creation of reusable componenets
-//Conditional Rendering: JSX supports conditional rendering through JavaScript expressions, allowing components to render different content based on conditions 
+/* 
+  Main Difference between HTML and JSX syntax :
+  -JSX:
+  +JSX is case-sensitive with camelCase properties.
+  +JSX allows JavaScript expressions.
+  +JSX requires a single root node.
+  +Attributes are camelcased (e.g., onClick).
+  +JSX can have custom components (e.g., <MyComponent />)
+  -Html:
+  +HTML is not case-sensitive.
+  +HTML does not have logic built-in; it requires a separate script
+  +HTML does not have this restriction
+  +Attributes are lowercase (e.g., click).
+  +HTML does not support custom components natively.
+*/
+
 
 function App() {
   return (
